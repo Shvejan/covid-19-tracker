2a8af9cf220eda6f374b45376a19511f30c171cf
Shvejan <49836470+Shvejan@users.noreply.github.com>
2020-04-22 00:51:09 +0530

    Updates

---
+++ precache-manifest.a1734f66242fe5eee7daf6be2c0ddb3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055ac480244069e3a6a5a3b49bba4f39",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "300eaf74914fd6a35bd2",
+    "url": "/covid-19-tracker/static/css/main.1534cdd2.chunk.css"
+  },
+  {
+    "revision": "bf9394111a0678ec38ac",
+    "url": "/covid-19-tracker/static/js/2.109c60f0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.109c60f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300eaf74914fd6a35bd2",
+    "url": "/covid-19-tracker/static/js/main.338266f3.chunk.js"
+  },
+  {
+    "revision": "163a8a02ef5425afd3d4",
+    "url": "/covid-19-tracker/static/js/runtime-main.85a6bd22.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/covd.d7265326.png"
+  }
+]);
